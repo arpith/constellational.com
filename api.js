681577463e4c78506b6583c6cbe79a6cc6791b79
Arpith Siromoney <arpith@constellational.com>
2013-11-05 04:14:04 +0000

    first commit

---
+++ api.js
@@ -0,0 +1,24 @@
+var AWS = require('aws-sdk')
+AWS.config.loadFromPath('./config.json')
+var s3 = new AWS.S3({params:{Bucket:'constellational.com'}})
+
+exports.get = function(callback) {
+  console.log("GET request to /api")
+  arguments[arguments.length-1]('Up')
+}
+exports.post = function(id) {
+  var requestBody = ''
+  , params = {}
+  , callback = arguments[arguments.length-1]
+  this.request.on('data', function(data) {
+    requestBody += data;
+    if(requestBody.length > 1e7) callback("Type less")
+  })
+  this.request.on('end', function() {
+    params = {Key:id, Body:requestBody, ContentType:'text/html'}
+    s3.putObject(params, function(e){
+      if (e) callback(e.message)
+      else callback('constellational.com.s3.amazonaws.com/'+id+' saved')
+    })
+  })
+}
---
+++ main.js
@@ -0,0 +1,1 @@
+require('fsjs')(8000)

fd487b601d602e9a313e9e1cd333753e410476d6
Arpith Siromoney <arpith@constellational.com>
2014-07-29 06:21:51 +0000

    respond with index.html

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 require('fsjs')(8000)
+var fs = require('fs')
+, html = fs.readFileSync('index.html',{encoding:'UTF-8'})
+exports.get = function() {
+  var callback = arguments[arguments.length-1]
+  callback(html)
+}
